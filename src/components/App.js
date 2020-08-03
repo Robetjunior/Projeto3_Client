29ed89a07d8aea5fece808724e2d682e73d2ddc7
Jose Roberto <juniorferreirarob@gmail.com>
2020-08-03 13:50:54 -0300

    logout & privateRoute done

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,8 @@ import Navbar from "./Navbar";
 
 //SIGNUP/LOGIN/LOGOUT
 import Signup from "./auth/Signup";
+import Logout from "./auth/Logout";
+import PrivateRoute from "./auth/PrivateRoute";
 
 function App() {
   const [loggedInUser, setLoggedInUser] = useState({});
@@ -22,6 +24,10 @@ function App() {
       <div className="contaienr-fluid w75 mt-5">
         <Switch>
           <Route path="/signup" component={Signup} />
+          <Route
+            path="/logout"
+            render={() => <Logout setUser={setLoggedInUser} />}
+          />
         </Switch>
       </div>
     </BrowserRouter>
