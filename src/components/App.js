362362848d665b11d78fa5ad85dac09452d56963
Jose Roberto <juniorferreirarob@gmail.com>
2020-08-03 14:53:55 -0300

    first steps homePage

--- src/components/App.js
+++ src/components/App.js
@@ -2,38 +2,43 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "../assets/styles/App.css";
 import Navbar from "./Navbar";
+
+import HomePage from "./HomePage";
+
 //SIGNUP/LOGIN/LOGOUT
-
 import Signup from "./auth/Signup";
-
-import Login from "./auth/Login"
-
+import Login from "./auth/Login";
 import Logout from "./auth/Logout";
 import PrivateRoute from "./auth/PrivateRoute";
 
-
 function App() {
   const [loggedInUser, setLoggedInUser] = useState({});
+
   useEffect(() => {
     const storedUser = JSON.parse(localStorage.getItem("loggedInUser") || '""');
     setLoggedInUser({ ...storedUser });
   }, []);
+
   return (
     <BrowserRouter>
-      <Navbar />
+      <Navbar loggedInUser={loggedInUser} />
       <div className="container-fluid w75 mt-5">
         <Switch>
+          <Route path="/" exact component={HomePage} />
+
           <Route path="/signup" component={Signup} />
 
-          <Route path="/login" render={() => (
+          <Route
+            path="/login"
+            render={() => (
               <Login user={loggedInUser} setUser={setLoggedInUser} />
             )}
-            />
+          />
+
           <Route
             path="/logout"
             render={() => <Logout setUser={setLoggedInUser} />}
           />
-
         </Switch>
       </div>
     </BrowserRouter>
