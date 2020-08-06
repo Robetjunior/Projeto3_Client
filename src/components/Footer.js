258b72d9318d9c317b76c2384fbdb165dd135e90
Jose Roberto <juniorferreirarob@gmail.com>
2020-08-06 11:24:03 -0300

    footer.js updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,14 @@
-import React from "react"
+import React from "react";
 
-const Footer = () =>{
-    return (
-        <footer className="footer">
-         
-         <h3 class="mt-5 mb-3 text-muted text-center">© Project-03 Developed by José Roberto, Eduardo Müller and Érick</h3> 
-          
-        </footer>
-      );
-    };
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <h1>RESOLVIDO. </h1>
+      <h3 class="mt-5 mb-3 text-muted text-center">
+        © Project-03 Developed by José Roberto, Eduardo Müller and Érick
+      </h3>
+    </footer>
+  );
+};
 
-
-export default Footer
+export default Footer;
